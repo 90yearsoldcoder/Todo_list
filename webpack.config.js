791fea5012762b3a3495b3e88c9aaf1c2b87c4b7
90yearsoldcoder <minty@bu.edu>
2024-01-28 22:32:02 -0500

    config webpack htmlwebpackplugin and file loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   context: path.resolve(__dirname),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "pathOrUrlWhenProductionBuild",
   },
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.(avif|png|jpg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {},
-  devtool: "source-map",
-  plugins: [],
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
 };
