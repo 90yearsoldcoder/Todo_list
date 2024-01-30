5cce9d9691c590c512628c11b0ff8857c1c1f347
90yearsoldcoder <minty@bu.edu>
2024-01-30 16:27:59 -0500

    test file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import "styles/outline.css";
+import "./styles/outline.css";
+import StorageAPI from "../src/modules/storage";
+import Thing from "../src/modules/thing";
+
+let t = new Thing("ProjectA", "No title", "For test", "2024/1/30", "High");
+StorageAPI.saveThing(t);
