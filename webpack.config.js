81021d67816a8be5aacd89b220c316d633344ec7
90yearsoldcoder <minty@bu.edu>
2024-02-02 19:53:40 -0500

    update dev env

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
     ],
   },
   resolve: {},
