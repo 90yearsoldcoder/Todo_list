899856ddb316922e227f9865fc7ac6a3bc76e875
90yearsoldcoder <minty@bu.edu>
2024-02-06 16:10:00 -0500

    Add functional mainpanel for project list

--- src/functions/updateProjectList.js
+++ src/functions/updateProjectList.js
@@ -1,18 +1,30 @@
 import StorageAPI from "../modules/storage";
+import loadMainPanel from "./loadMainPanel";
+
+const remove_active = () => {
+  let parentElement = document.querySelector(".sidebar");
+  parentElement.querySelectorAll("*").forEach((child) => {
+    child.classList.remove("sidebar-active");
+  });
+};
 
 const updateProjectList = (container) => {
   let projectList = StorageAPI.get_ProjectsList();
   for (let project of projectList) {
     let project_div = document.createElement("div");
-    project_div.classList.add = "sidebar_title";
+    project_div.classList.add("sidebar_title");
     project_div.innerHTML = `
-    <div class="sidebar_title">
         <i class="fa-solid fa-bars-progress fa-lg"></i>
         <div>${project}</div>
-    </div>
     `;
 
     //Todo: add a lissener for the button
+    project_div.addEventListener("click", () => {
+      let todolist = StorageAPI.retrieve_todolist_byProject(project);
+      remove_active();
+      project_div.classList.add("sidebar-active");
+      loadMainPanel(todolist);
+    });
 
     container.appendChild(project_div);
   }
